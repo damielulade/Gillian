c8168035ad21fffc8a33951a30de0e5e75218540
Dami Elulade <delulade72@gmail.com>
2025-04-15 12:12:39 +0100

    refactor: Change structure of Types to move lifter state into main module

--- Gillian-JS/lib/Debugging/JSLifter.ml
+++ Gillian-JS/lib/Debugging/JSLifter.ml
@@ -6,20 +6,47 @@ module Make
     (Gil : Gillian.Debugger.Lifter.Gil_fallback_lifter.Gil_lifter_with_state)
     (V : Gillian.Abstraction.Verifier.S with type annot = PC.Annot.t) =
 struct
-  module Partial_cmds = PartialCmds
-  module More_types = Types.Make (Gil) (V)
-  include More_types
-  include Types
+  module Lifter_Types = Types.Make (Gil) (V)
+  module Fallback_Gil_lifter = Gil.Lifter
+  module Partial_cmds = PartialCmds.Make (Lifter_Types)
+  module Prog = Gil_syntax.Prog
+  include Lifter_Types
+
+  type t = {
+    proc_name : string;
+    gil_state : Fallback_Gil_lifter.t; [@to_yojson Fallback_Gil_lifter.dump]
+    tl_ast : tl_ast; [@to_yojson fun _ -> `Null]
+    prog : (annot, int) Prog.t; [@to_yojson fun _ -> `Null]
+    partial_cmds : Partial_cmds.t;
+    map : map;
+    func_return_map : (id, string * int ref) Hashtbl.t;
+    mutable func_return_count : int;
+  }
+  [@@deriving to_yojson]
+
   include StepFuncs
   include VarFuncs
   include UtilFuncs
   open Exec_map
 
-  let init ~proc_name ~all_procs:_ tl_ast _prog =
-    let _gil_state = Gil.get_state () in
-    let+ _tl_ast = tl_ast in
-    (* let partial_cmds = Partial_cmds.init () in *)
-    failwith proc_name
+  let init ~proc_name ~all_procs:_ tl_ast prog =
+    let gil_state = Gil.get_state () in
+    let+ tl_ast = tl_ast in
+    let partial_cmds = Partial_cmds.init () in
+    let state =
+      {
+        proc_name;
+        gil_state;
+        tl_ast;
+        prog;
+        partial_cmds;
+        map = Exec_map.make ();
+        func_return_map = Hashtbl.create 0;
+        func_return_count = 0;
+      }
+    in
+    let finish_init = failwith proc_name in
+    (state, finish_init)
 
   let init_exn ~proc_name ~all_procs tl_ast prog =
     match init ~proc_name ~all_procs tl_ast prog with
@@ -27,10 +54,6 @@ struct
     | Some x -> x
 
   let dump = to_yojson
-
-  (* Interface function
-     Gives a list of matches that occurred at the specified command.
-  *)
   let get_matches_at_id id { map; _ } = (get_exn map id).data.matches
 
   let memory_error_to_exception_info _info : exception_info =
@@ -39,7 +62,3 @@ struct
   let parse_and_compile_files ~entrypoint files =
     PC.parse_and_compile_files files |> Result.map (fun r -> (r, entrypoint))
 end
-
-(* module JS_matching_map = struct
-
-   end *)
